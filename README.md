754d18a64351efc99c45919ac3cb39bcda91008b
Rui Carmo <rui.carmo@gmail.com>
2014-10-18 10:58:32 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# sapo-broker-golang
+
+This is a simple (WIP) Golang client for [sapo-broker][sb] using the JSON transport.
+
+[sb]: https://github.com/sapo/sapo-broker
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "fmt"
+    "encoding/json"
+)
+
+type (
+    BrokerMessagePayload struct {
+        Payload     *string `json:"payload,omitempty"`
+    }
+    BrokerActionInfo struct {
+        Destination *string `json:"destination"`
+        ActionType string `json:"action_type"`
+        DestinationType string `json:"destination_type"`
+        Message *BrokerMessagePayload `json:"message,omitempty"`
+    }
+    BrokerAction struct {
+        PublishInfo *BrokerActionInfo `json:"publish,omitempty"`
+        SubscribeInfo *BrokerActionInfo `json:"subscribe,omitempty"`
+    }
+    BrokerMessage struct {
+        Action  *BrokerAction  `json:"action"`
+    }
+)
+
+func NewPublishMessage(topic string, message *string) *BrokerMessage {
+    return &BrokerMessage{
+        Action: &BrokerAction{
+            PublishInfo: &BrokerActionInfo{
+                ActionType: "SUBSCRIBE",
+                Destination: &topic,
+                DestinationType: "TOPIC",
+                Message: &BrokerMessagePayload{
+                    Payload: message,
+                },
+            },
+        },
+    }
+}
+
+func NewSubscribeMessage(topic string) *BrokerMessage {
+    return &BrokerMessage{
+        Action: &BrokerAction{
+            SubscribeInfo: &BrokerActionInfo{
+                ActionType: "SUBSCRIBE",
+                Destination: &topic,
+                DestinationType: "TOPIC",
+                Message: nil,
+            },
+        },
+    }
+}
+
+func main() {
+    /*
+	conn, err := net.Dial("tcp", "broker.labs.sapo.pt:3323")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response: %+v\n", res)
+    */
+    payload := "Hello world!"
+    msg := NewPublishMessage("/sapo/test/announcements", &payload);
+    b, _ := json.Marshal(*msg)
+    fmt.Println(string(b))
+    msg = NewSubscribeMessage("/sapo/.*");
+    b, _ = json.Marshal(*msg)
+    fmt.Println(string(b))
+}
