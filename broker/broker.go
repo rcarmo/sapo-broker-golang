e903676319a23a2b49f7a65d4ddc518c128d5428
Rui Carmo <rui.carmo@gmail.com@git config --global user.email rui.carmo@gmail.comgit config --global user.email rui.carmo@gmail.com@>
2014-10-26 21:35:49 +0000

    split off library stub

---
+++ broker/broker.go
@@ -0,0 +1,70 @@
+package broker
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+)
+
+type (
+	BrokerMessagePayload struct {
+		Payload *string `json:"payload,omitempty"`
+	}
+	BrokerActionInfo struct {
+		Destination     *string               `json:"destination"`
+		ActionType      string                `json:"action_type"`
+		DestinationType string                `json:"destination_type"`
+		Message         *BrokerMessagePayload `json:"message,omitempty"`
+	}
+	BrokerAction struct {
+		PublishInfo   *BrokerActionInfo `json:"publish,omitempty"`
+		SubscribeInfo *BrokerActionInfo `json:"subscribe,omitempty"`
+	}
+	BrokerMessage struct {
+		Action *BrokerAction `json:"action"`
+	}
+)
+
+func NewPublishMessage(topic string, message *string) *BrokerMessage {
+	return &BrokerMessage{
+		Action: &BrokerAction{
+			PublishInfo: &BrokerActionInfo{
+				ActionType:      "SUBSCRIBE",
+				Destination:     &topic,
+				DestinationType: "TOPIC",
+				Message: &BrokerMessagePayload{
+					Payload: message,
+				},
+			},
+		},
+	}
+}
+
+func NewSubscribeMessage(topic string) *BrokerMessage {
+	return &BrokerMessage{
+		Action: &BrokerAction{
+			SubscribeInfo: &BrokerActionInfo{
+				ActionType:      "SUBSCRIBE",
+				Destination:     &topic,
+				DestinationType: "TOPIC",
+				Message:         nil,
+			},
+		},
+	}
+}
+
+func pack(msg *BrokerMessage) []byte {
+	b, _ := json.Marshal(*msg)
+	buf := new(bytes.Buffer)
+	var data = []interface{}{
+		uint16(3),     // JSON transport
+		uint16(0),     // version
+		int32(len(b)), // payload length
+		b,
+	}
+
+	for _, item := range data {
+		binary.Write(buf, binary.BigEndian, item)
+	}
+	return buf.Bytes()
+}
--- main.go
+++ main.go
@@ -1,74 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"encoding/json"
+	"./broker"
+    "encoding/json"
 	"fmt"
 )
-
-type (
-	BrokerMessagePayload struct {
-		Payload *string `json:"payload,omitempty"`
-	}
-	BrokerActionInfo struct {
-		Destination     *string               `json:"destination"`
-		ActionType      string                `json:"action_type"`
-		DestinationType string                `json:"destination_type"`
-		Message         *BrokerMessagePayload `json:"message,omitempty"`
-	}
-	BrokerAction struct {
-		PublishInfo   *BrokerActionInfo `json:"publish,omitempty"`
-		SubscribeInfo *BrokerActionInfo `json:"subscribe,omitempty"`
-	}
-	BrokerMessage struct {
-		Action *BrokerAction `json:"action"`
-	}
-)
-
-func NewPublishMessage(topic string, message *string) *BrokerMessage {
-	return &BrokerMessage{
-		Action: &BrokerAction{
-			PublishInfo: &BrokerActionInfo{
-				ActionType:      "SUBSCRIBE",
-				Destination:     &topic,
-				DestinationType: "TOPIC",
-				Message: &BrokerMessagePayload{
-					Payload: message,
-				},
-			},
-		},
-	}
-}
-
-func NewSubscribeMessage(topic string) *BrokerMessage {
-	return &BrokerMessage{
-		Action: &BrokerAction{
-			SubscribeInfo: &BrokerActionInfo{
-				ActionType:      "SUBSCRIBE",
-				Destination:     &topic,
-				DestinationType: "TOPIC",
-				Message:         nil,
-			},
-		},
-	}
-}
-
-func pack(msg *BrokerMessage) []byte {
-	b, _ := json.Marshal(*msg)
-	buf := new(bytes.Buffer)
-	var data = []interface{}{
-		uint16(3),     // JSON transport
-		uint16(0),     // version
-		int32(len(b)), // payload length
-		b,
-	}
-
-	for _, item := range data {
-		binary.Write(buf, binary.BigEndian, item)
-	}
-	return buf.Bytes()
-}
 
 func main() {
 	/*
@@ -80,12 +16,10 @@ func main() {
 		fmt.Printf("Response: %+v\n", res)
 	*/
 	payload := "Hello world!"
-	msg := NewPublishMessage("/sapo/test/announcements", &payload)
+	msg := broker.NewPublishMessage("/sapo/test/announcements", &payload)
 	b, _ := json.Marshal(*msg)
 	fmt.Println(string(b))
-	msg = NewSubscribeMessage("/sapo/.*")
+	msg = broker.NewSubscribeMessage("/sapo/.*")
 	b, _ = json.Marshal(*msg)
 	fmt.Println(string(b))
-	b = pack(msg)
-	fmt.Printf("%x\n", b)
 }
